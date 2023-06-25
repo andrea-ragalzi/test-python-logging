e02b5c6b737b36532e8aae5baf3689ef01105bc1
andrea-ragalzi <andrea.ragalzi.code@gmail.com>
2023-06-25 14:51:03 +0200

    modules

---
+++ utils/apple.py
@@ -0,0 +1,8 @@
+# apple.py
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def eat_apple():
+    logger.info('I ate an apple')
---
+++ utils/pear.py
@@ -0,0 +1,8 @@
+# pear.py
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def eat_pear():
+    logger.info('I ate a pear')
